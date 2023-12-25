f92388aa1b9cfcc478fcbb908fa74c9e48509766
Domains18 <gibsonsgibson88@gmail.com>
2023-12-26 01:39:55 +0300

    golang auth controllers and mysql query

---
+++ controllers/authController.go
@@ -0,0 +1,65 @@
+package controllers
+
+import (
+	"github.com/Domains18/ReactGoAuth/database"
+	"github.com/Domains18/ReactGoAuth/models"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gofiber/fiber"
+	"golang.org/x/crypto/bcrypt"
+	"strconv"
+	"time"
+)
+
+const secretKey = "authSecret"
+
+func Register(c *fiber.Ctx) error {
+	var data map[string]string
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
+	user := models.User{
+		Name:     data["name"],
+		Email:    data["email"],
+		Password: string(password),
+	}
+	database.DB.Create(&user)
+	return c.JSON(user)
+}
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	var user models.User
+	database.DB.Where("email = ?", data["email"]).First(&user)
+	if user.Id == 0 {
+		c.Status(fiber.StatusNotFound)
+		return c.JSON(fiber.Map{"message": "user not found"})
+	}
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(data["password"])); err != nil {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{"message": "User not found"})
+	}
+	claims := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.StandardClaims{
+		Issuer:    strconv.Itoa(int(user.Id)),
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+	})
+	token, err := claims.SignedString([]byte(secretKey))
+	if err != nil{
+		c.Status(fiber.StatusInternalServerError)
+		return c.JSON(fiber.Map{"message": "internal server error"})
+	}
+
+	cookie := fiber.Cookie{
+		Name: "jwt",
+		Value: token,
+		Expires: time.Now().Add(time.Hour * 24),
+		HTTPOnly: true,
+	}
+	c.Cookie(&cookie)
+
+	return c.JSON(fiber.Map{"message": "success"})
+}
--- go.mod
+++ go.mod
@@ -2,11 +2,26 @@ module github.com/Domains18/ReactGoAuth
 
 go 1.21.3
 
-require gorm.io/driver/mysql v1.5.2
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gofiber/fiber v1.14.6
+	golang.org/x/crypto v0.17.0
+	gorm.io/driver/mysql v1.5.2
+	gorm.io/gorm v1.25.2-0.20230530020048-26663ab9bf55
+)
 
 require (
+	github.com/andybalholm/brotli v1.0.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/gofiber/utils v0.0.10 // indirect
+	github.com/gorilla/schema v1.1.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	gorm.io/gorm v1.25.2-0.20230530020048-26663ab9bf55 // indirect
+	github.com/klauspost/compress v1.10.7 // indirect
+	github.com/mattn/go-colorable v0.1.7 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.16.0 // indirect
+	github.com/valyala/tcplisten v0.0.0-20161114210144-ceec8f93295a // indirect
+	golang.org/x/sys v0.15.0 // indirect
 )
